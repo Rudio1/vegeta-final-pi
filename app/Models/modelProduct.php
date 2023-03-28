14192203f1842744f927941524dc8b2268030747
Rudio1 <guilhermerudio6@gmail.com>
2023-03-28 20:48:06 +0044

    Finalizando modelagem do banco de produtos + model

---
+++ app/Models/modelProduct.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class modelProduct extends Model
+{
+    use HasFactory;
+
+    protected $table = 'models';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nome',
+    ];
+
+    public function class(){
+        return $this->hasMany(Product::class);
+    }
+}
